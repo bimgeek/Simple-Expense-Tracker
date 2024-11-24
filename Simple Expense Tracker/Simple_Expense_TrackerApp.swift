cdfe10144ddbd65b83c2a620ba05a34e1f4a8b06
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2024-11-24 23:45:19 +0300

    Initial Commit

---
+++ Simple Expense Tracker/Simple_Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Expense_TrackerApp.swift
+//  Simple Expense Tracker
+//
+//  Created by Mucahit Bilal GOKER on 24.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple Expense TrackerTests/Simple_Expense_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Expense_TrackerTests.swift
+//  Simple Expense TrackerTests
+//
+//  Created by Mucahit Bilal GOKER on 24.11.2024.
+//
+
+import Testing
+@testable import Simple_Expense_Tracker
+
+struct Simple_Expense_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
