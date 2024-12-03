0e1c2d88bacbd4a078137f9cc2a8f2ca585653f9
Mucahit Bilal GOKER <mucahitbgoker@gmail.com>
2024-12-03 21:13:24 +0300

    Merged Enums into a single file

--- Simple Expense Tracker/ContentView.swift
+++ Simple Expense Tracker/ContentView.swift
@@ -7,42 +7,6 @@
 
 import SwiftUI
 
-// Add this enum definition at the top level, before ContentView
-enum DateOption: Equatable {
-    case yesterday
-    case today
-    case custom(Date)
-    
-    static func == (lhs: DateOption, rhs: DateOption) -> Bool {
-        switch (lhs, rhs) {
-        case (.yesterday, .yesterday):
-            return true
-        case (.today, .today):
-            return true
-        case (.custom(let date1), .custom(let date2)):
-            return Calendar.current.isDate(date1, inSameDayAs: date2)
-        default:
-            return false
-        }
-    }
-    
-    var date: Date {
-        switch self {
-        case .yesterday:
-            return Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()
-        case .today:
-            return Date()
-        case .custom(let date):
-            return date
-        }
-    }
-}
-
-// Add TabSelection enum at the top level, next to DateOption
-enum TabSelection {
-    case home, log, insights, settings
-}
-
 struct ContentView: View {
     @StateObject private var expenseManager = ExpenseManager()
     @State private var showingAddExpense = false
